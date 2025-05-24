80af3b90e573ec941ac92271cf62af320c6295f6
A-fethi <fethiabderrahmane1@gmail.com>
2025-05-24 12:05:42 +0100

    recorrecting

--- static/components/circlechart.js
+++ static/components/circlechart.js
@@ -3,10 +3,23 @@ import { getExtension, getColor } from "./utils.js"
 
 export const circleChart = (projects) => {
     const svg = document.getElementById("xpPieChart");
-    const totalXP = projects.reduce((sum, p) => sum + p.amount, 0);
+    const projectSums = {};
+    projects.forEach(project => {
+        const name = project.path.split('/').pop();
+        projectSums[name] = (projectSums[name] || 0) + project.amount;
+    });
+
+    const filteredProjects = Object.entries(projectSums)
+        .filter(([name, amount]) => amount > 0)
+        .map(([name, amount]) => ({
+            name,
+            amount
+        }));
+
+    const totalXP = filteredProjects.reduce((sum, p) => sum + p.amount, 0);
     let offset = 0;
 
-    projects.forEach((project) => {
+    filteredProjects.forEach((project) => {
         const percentage = project.amount / totalXP;
         const circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
@@ -22,7 +35,7 @@ export const circleChart = (projects) => {
         circle.addEventListener("mousemove", (e) => {
             const tooltip = document.getElementById("tooltip");
             tooltip.innerHTML = `
-                <strong>Project:</strong> ${project.path.split('/').pop()}<br>
+                <strong>Project:</strong> ${project.name}<br>
                 <strong>XP:</strong> ${getExtension(project.amount)}<br>
                 <strong>Percentage:</strong> ${(percentage * 100).toFixed(2)}%
             `;
